b61c24e0b4340e56285de781daeca5213bb90d22
Alexey Storozhuk <alexcandoitt@gmail.com>
2021-03-07 13:59:16 +0200

    Done hw

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+defaultModules.set(PNotifyMobile, {});
+
 import './sass/styles.scss';
 var debounce = require('lodash.debounce');
 
+import countriesListTpl from './templates/country-list.hbs';
+import countriesCardTpl from './templates/country-card.hbs';
+
 const refs = {
-  searchInput: document.querySelector('.input-js'),
+  searchInput: document.querySelector('.input'),
+  container: document.querySelector('.countries'),
 };
 
 refs.searchInput.addEventListener('input', debounce(onSearch, 500));
@@ -10,12 +21,23 @@ refs.searchInput.addEventListener('input', debounce(onSearch, 500));
 function onSearch({ target: { value } }) {
   fetchCountry(value)
     .then(response => {
-      console.log(
-        PNotify.error({
-          title: response.length,
-          text: 'Something terrible happened.',
-        }),
-      );
+      refs.container.innerHTML = '';
+
+      if (response.length === 1) {
+        refs.container.insertAdjacentHTML(
+          'beforeend',
+          countriesCardTpl(response),
+        );
+      } else if (response.length >= 2 && response.length <= 10) {
+        refs.container.insertAdjacentHTML(
+          'beforeend',
+          countriesListTpl(response),
+        );
+      } else if (response.length > 10) {
+        error({
+          text: 'Too many matches found. Please enter a more specific query!',
+        });
+      }
     })
     .catch();
 }
